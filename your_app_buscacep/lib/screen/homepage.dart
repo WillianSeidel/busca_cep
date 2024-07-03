abce53dec1cc2b0422e354add8384c87b7be54aa
WillianSeidel <willianrobertoseidel@gmail.com>
2024-07-03 14:31:38 -0300

    home screen

---
+++ your_app_buscacep/lib/screen/homepage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Digite o CEP desejado:',
+                style: TextStyle(fontSize: 30.0)),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    errorBorder: OutlineInputBorder(),
+                    labelText: 'CEP',
+                  )),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Aqui você pode adicionar a ação desejada quando o botão for pressionado
+          print('CEP digitado: ${_controller.text}');
+        },
+        tooltip: 'Search',
+        child: const Icon(Icons.search),
+      ),
+    );
+  }
+}
